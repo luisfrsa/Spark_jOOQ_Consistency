4d7a662f0adba1c57d9fb7cc950e050166b75253
Luis <luis.alves@db1.com.br>
2018-06-16 10:18:13 -0300

    First commit, introducing jooq and spark

---
+++ src/main/java/domain/User.java
@@ -0,0 +1,41 @@
+package domain;
+
+public class User {
+
+    Long id;
+    String name;
+    String email;
+
+    public User(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public User setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+}
